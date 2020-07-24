4b1438bab326a2ef078628e65b1dd768e11b4382
michaela-elise <michaelaervin@lavorotechnologies.com>
2020-07-23 18:37:20 -0600

    remove links

--- public/app/core/components/sidemenu/BottomNavLinks.tsx
+++ public/app/core/components/sidemenu/BottomNavLinks.tsx
@@ -6,7 +6,6 @@ import { NavModelItem } from '@grafana/data';
 import { Icon, IconName } from '@grafana/ui';
 import { CoreEvents } from 'app/types';
 import { OrgSwitcher } from '../OrgSwitcher';
-import { getFooterLinks } from '../Footer/Footer';
 
 export interface Props {
   link: NavModelItem;
@@ -41,12 +40,6 @@ export default class BottomNavLinks extends PureComponent<Props, State> {
       margin-right: 8px;
     `;
 
-    let children = link.children || [];
-
-    if (link.id === 'help') {
-      children = getFooterLinks();
-    }
-
     return (
       <div className="sidemenu-item dropdown dropup">
         <a href={link.url} className="sidemenu-link" target={link.target}>
@@ -77,17 +70,6 @@ export default class BottomNavLinks extends PureComponent<Props, State> {
           )}
 
           {showSwitcherModal && <OrgSwitcher onDismiss={this.toggleSwitcherModal} />}
-
-          {children.map((child, index) => {
-            return (
-              <li key={`${child.text}-${index}`}>
-                <a href={child.url} target={child.target} rel="noopener">
-                  {child.icon && <Icon name={child.icon as IconName} className={subMenuIconClassName} />}
-                  {child.text}
-                </a>
-              </li>
-            );
-          })}
 
           {link.id === 'help' && (
             <li key="keyboard-shortcuts">
